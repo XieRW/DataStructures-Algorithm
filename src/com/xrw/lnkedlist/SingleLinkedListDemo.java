bd93e629bd1adcd6c0b62f84508908a98383089b
XieRW <1429382875@qq.com>
2021-04-08 13:38:55 +0800

    1．从键盘输入任意的10个随机正整数，生成一个单链表；
    2．输出显示该单链表每个结点的元素值；
    
    3．依次访问单链表的每个元素，如果是奇数，将其删除， 如果是偶数，就在其前面插入-1。
    
    4．输出显示该单链表每个结点的元素值；

--- src/com/xrw/lnkedlist/SingleLinkedListDemo.java
+++ src/com/xrw/lnkedlist/SingleLinkedListDemo.java
@@ -1,5 +1,9 @@
 package com.xrw.lnkedlist;
 
+import sun.plugin.dom.core.CoreConstants;
+
+import java.util.Scanner;
+
 /**
  * @program: DataStructures
  * @description: 单链表
@@ -9,7 +13,27 @@ package com.xrw.lnkedlist;
 public class SingleLinkedListDemo {
 
     public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        SingleLinkedList singleLinkedList = new SingleLinkedList();
+        for (int i = 0; i < 10; i++) {
+            System.out.printf("请输入第%d个节点的课程编号\t",i+1);
+            int no = in.nextInt();
+            System.out.printf("请输入第%d个节点的课程名称\t",i+1);
+            String name = in.next();
+            System.out.printf("请输入第%d个节点的课程难度\t",i+1);
+            String hard = in.next();
 
+            Course course = new Course(no, name, hard);
+
+            singleLinkedList.add(course);
+            System.out.println("链表每个节点的元素为：");
+            singleLinkedList.list();
+        }
+        System.out.println("整理单链表：依次访问单链表的每个元素，如果是奇数，将其删除， 如果是偶数，就在其前面插入-1");
+
+        singleLinkedList.clear();
+        System.out.println("整理后的链表为：");
+        singleLinkedList.list();
     }
 }
 
@@ -18,9 +42,9 @@ public class SingleLinkedListDemo {
  * @Author: 谢荣旺
  * @Date: 2021/4/1
  */
-class SingleLinkedList{
+class SingleLinkedList {
     //创建一个头结点，不存放数据
-    Course headNode = new Course(0,"","");
+    Course headNode = new Course(0, "", "");
 
     /**
      * @Description: 获取头结点
@@ -28,23 +52,23 @@ class SingleLinkedList{
      * @Author: 谢荣旺
      * @Date: 2021/4/1
      */
-    public Course getHeadNode(){
+    public Course getHeadNode() {
         return headNode;
     }
 
     /**
+     * @param course: 要加入的节点
      * @Description: 默认在链表最后加入一个节点
      * 实现思路：
      * 1、用一个辅助节点temp去遍历链表，temp的初始值temp=headNode
      * 2、循环移动temp:temp = temp.next,如果temp.next为空则将要加入的节点插入：temp.next = course
-     * @param course: 要加入的节点
      * @Author: 谢荣旺
      * @Date: 2021/4/1
      */
-    public void add(Course course){
+    public void add(Course course) {
         Course temp = headNode;
-        while (true){
-            if (temp.next == null){
+        while (true) {
+            if (temp.next == null) {
                 break;
             }
             temp = temp.next;
@@ -53,6 +77,7 @@ class SingleLinkedList{
     }
 
     /**
+     * @param course: 要加入的节点
      * @Description: 按照序号加入一个节点
      * 实现思路：
      * 1、用一个辅助节点temp去遍历链表，temp的初始值temp=headNode
@@ -60,22 +85,20 @@ class SingleLinkedList{
      * 3.1、如果temp.next == null，那么将course加入temp.next,退出循环
      * 3.2、如果temp.next.no == course.no，那么返回错误提示,退出循环
      * 3.3，如果temp.next.no > course.no，那么将course插入入到temp和temp.next之间,course.next = temp.next,temp.next = course,退出循环
-     * @param course: 要加入的节点
      * @Author: 谢荣旺
      * @Date: 2021/4/1
      */
-    public void addByOrder(Course course){
+    public void addByOrder(Course course) {
         Course temp = headNode;
-
-        while (true){
-            if (temp.next == null){
+        while (true) {
+            if (temp.next == null) {
                 break;
             }
-            if (temp.next.no == course.no){
-                System.out.printf("编号%d的课程已存在，不能重复插入~\n",course.no);
+            if (temp.next.no == course.no) {
+                System.out.printf("编号%d的课程已存在，不能重复插入~\n", course.no);
                 break;
             }
-            if (temp.next.no > course.no){
+            if (temp.next.no > course.no) {
                 course.next = temp.next;
                 temp.next = course;
                 break;
@@ -85,43 +108,126 @@ class SingleLinkedList{
     }
 
     /**
-     * @Description: 通过节点的编号更新节点
      * @param course: 要更新的节点
+     * @Description: 通过节点的编号更新节点
+     * 实现思路：
+     * 1、用一个辅助节点temp去遍历链表，temp的初始值temp=headNode.next
+     * 2、如果temp == null，则链表为空,返回错误提示
+     * 3、循环移动temp:temp = temp.next
+     * 4.1如果temp == null，那么返回错误提示，退出循环
+     * 4.2如果temp.no == course.no,那么temp.name = course.name，temp.hard = course.hard
      * @Author: 谢荣旺
      * @Date: 2021/4/1
      */
-    public void update(Course course){
-
+    public void update(Course course) {
+        Course temp = headNode.next;
+        if (temp == null) {
+            System.out.printf("链表为空");
+        }
+        while (true) {
+            if (temp == null) {
+                System.out.printf("编号为%d的数据不存在", course.no);
+                break;
+            }
+            if (temp.no == course.no) {
+                temp.name = course.name;
+                temp.hard = course.hard;
+            }
+            temp = temp.next;
+        }
     }
 
     /**
-     * @Description: 通过编号删除一个节点
      * @param no: 要删除节点的编号
+     * @Description: 通过编号删除一个节点
+     * 实现思路：
+     * 1、用一个辅助节点temp去遍历链表，temp的初始值temp=headNode
+     * 2.1如果temp.next == null，那么返回错误提示，退出循环
+     * 2.2如果temp.next.no == no,那么temp.next = temp.next.next
+     * 3、循环移动temp:temp = temp.next
      * @Author: 谢荣旺
      * @Date: 2021/4/1
      */
-    public void delete(int no){
-
+    public void delete(int no) {
+        Course temp = headNode;
+        while (true) {
+            if (temp.next == null) {
+                System.out.printf("要删除的编号为%d的节点不存在",no);
+                break;
+            }
+            if (temp.next.no == no) {
+                temp.next = temp.next.next;
+                break;
+            }
+            temp = temp.next;
+        }
     }
 
     /**
      * @Description: 遍历显示每个节点的数据
+     * 实现思路：用一个辅助节点temp去遍历
      * @Author: 谢荣旺
      * @Date: 2021/4/1
      */
-    public void list(){
-
+    public void list() {
+        Course temp = headNode.next;
+        if (temp == null){
+            System.out.println("链表为空~~~");;
+        }
+        while (true){
+            if (temp == null){
+                break;
+            }
+            System.out.println(temp);
+            temp = temp.next;
+        }
     }
 
     /**
-     * @Description: 通过课程名称查找节点
      * @param name: 课程名称
+     * @Description: 通过课程名称查找节点
      * @return: 查找到的节点
      * @Author: 谢荣旺
      * @Date: 2021/4/1
      */
-    public Course searchByName(String name){
-        return new Course(0,"","");
+    public Course searchByName(String name) {
+        Course temp = headNode.next;
+        if (temp == null){
+            throw new RuntimeException("课程不存在！");
+        }
+        while (true){
+            if (temp == null){
+                throw new RuntimeException("课程不存在！");
+            }
+            if (temp.name.contains(name)){
+                return temp;
+            }
+            temp = temp.next;
+        }
+    }
+
+    /**
+     * @Description: 整理单链表：依次访问单链表的每个元素，如果是奇数，将其删除， 如果是偶数，就在其前面插入-1
+     * @Author: 谢荣旺
+     * @Date: 2021/4/8
+     */
+    public void clear(){
+        Course temp = headNode;
+
+        while (true){
+            if (temp.next == null){
+                return;
+            }
+            if (temp.next.no%2 == 1){
+                temp.next = temp.next.next;
+            }else if (temp.next.no%2 == 0){
+                Course course = new Course(-1,"默认","default");
+                course.next = temp.next;
+                temp.next = course;
+                temp = temp.next;
+            }
+            temp = temp.next;
+        }
     }
 }
 
@@ -130,7 +236,7 @@ class SingleLinkedList{
  * @Author: 谢荣旺
  * @Date: 2021/4/1
  */
-class Course{
+class Course {
     //课程编号
     int no;
     //课程名称
@@ -143,7 +249,7 @@ class Course{
     /**
      * @Description: 构造函数
      */
-    public Course(int no,String name,String hard){
+    public Course(int no, String name, String hard) {
         this.no = no;
         this.name = name;
         this.hard = hard;

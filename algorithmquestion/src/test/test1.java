21faff134a21e284c50410495c5c61965f52f9c3
谢小鱼的笔记本 <1429382875@qq.com>
2021-08-14 16:36:39 +0800

    小算法

---
+++ algorithmquestion/src/test/test1.java
@@ -0,0 +1,19 @@
+package test;
+
+import java.util.Scanner;
+
+public class test1 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        while(in.hasNextInt()){
+            double n = in.nextInt();
+            double m = in.nextInt();
+            double sum=0;
+            for (int i = 0; i < m; i++) {
+                sum += n;
+                n = Math.sqrt(n);
+            }
+            System.out.printf("%.2f\n",sum);
+        }
+    }
+}
---
+++ algorithmquestion/src/test/tset2.java
@@ -0,0 +1,27 @@
+package test;
+
+import java.util.Scanner;
+
+public class tset2 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        while (in.hasNext()){
+            int m = in.nextInt();
+            int n = in.nextInt();
+            boolean exit = false;
+            for (int i = m;i<=n;i++){
+                int s = i;
+                int sum = (s/100)*(s/100)*(s/100)+(s%100/10)*(s%100/10)*(s%100/10)+(s%10)*(s%10)*(s%10);
+                if (sum == s){
+                    exit = true;
+                    System.out.printf("%d\t",s);
+                }
+            }
+            if (exit){
+                System.out.println("");
+            }else {
+                System.out.println("no");
+            }
+        }
+    }
+}

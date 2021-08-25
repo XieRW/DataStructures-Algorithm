c89f811c0c8ab82e2207ca117a20b83b52e40be1
谢小鱼的笔记本 <1429382875@qq.com>
2021-08-25 18:04:36 +0800

    测试一下线程池、拒绝策略的源码分析

---
+++ algorithmquestion/src/test/date210825/Main1.java
@@ -0,0 +1,13 @@
+package test.date210825;
+
+import java.util.concurrent.ThreadPoolExecutor;
+
+/**
+ * 测试一下线程池、拒绝策略
+ */
+public class Main1 {
+
+    public static void main(String[] args) {
+
+    }
+}

a0ad2153e9aeffdfdbdca76be95847f29f1c65ed
XieRW <1429382875@qq.com>
2021-08-28 21:39:53 +0800

    测试下东方财富证券的接口密码的加密位数

---
+++ algorithmquestion/src/test/date210827/Main1.java
@@ -0,0 +1,14 @@
+package test.date210827;
+
+/**
+ * @program: DataStructures
+ * @description: 测试下东方财富证券的接口
+ * @author: 谢荣旺 1429382875@qq.com
+ * @create: 2021-08-27 22:07
+ **/
+public class Main1 {
+    public static void main(String[] args) {
+        String s = "Y3LZVsMusUtILndwKazGIR8gg45mQd5KUB1gyqnpfbq8gUYmZCk1cSLpAPTliqmWPT8mx2q60k8MZ+MevxwVICbgxoChbHypAcANyaP1+MSpEO99gnyV13ydQkBLocrA3fmeKd6iHHvbeK2oag4o0RVPparWCRzgEQlTbD5rURI=";
+        System.out.println(s.length());
+    }
+}

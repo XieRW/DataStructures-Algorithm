f3ce9785da03e70cc9ddf63e31c0f86170e33caa
XieRW <1429382875@qq.com>
2021-09-22 22:38:42 +0800

    剑指offer第四题

--- algorithmquestion/src/com/xrw/swordfingeroffer/Jz4.java
+++ algorithmquestion/src/com/xrw/swordfingeroffer/Jz4.java
@@ -1,5 +1,7 @@
 package com.xrw.swordfingeroffer;
 
+import java.util.Arrays;
+
 /**
  * @program: DataStructures
  * @description: 剑指offer第四题
@@ -10,5 +12,34 @@ public class Jz4 {
 
     public static void main(String[] args) {
 
+    }
+
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public class Solution {
+        public TreeNode reConstructBinaryTree(int[] pre, int[] vin) {
+            if (pre.length==0 || vin.length==0){
+                return null;
+            }
+            TreeNode root = new TreeNode(pre[0]);
+            //在中序数组中找根
+            for (int i = 0; i < vin.length; i++) {
+                if (vin[i] == pre[0]){
+                    root.left = reConstructBinaryTree(Arrays.copyOfRange(pre,1,i+1),Arrays.copyOfRange(vin,0,i));
+                    root.right = reConstructBinaryTree(Arrays.copyOfRange(pre,i+1,pre.length),Arrays.copyOfRange(vin,i+1,vin.length));
+                    break;
+                }
+            }
+            return root;
+        }
     }
 }

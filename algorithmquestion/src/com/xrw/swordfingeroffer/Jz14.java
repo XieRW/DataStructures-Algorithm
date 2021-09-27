e6b6f73941a3cd0b26d0a73be507651753ecbebf
XieRW <1429382875@qq.com>
2021-09-27 14:11:19 +0800

    剑指offer第十四题

--- algorithmquestion/src/com/xrw/swordfingeroffer/Jz14.java
+++ algorithmquestion/src/com/xrw/swordfingeroffer/Jz14.java
@@ -22,7 +22,7 @@ public class Jz14 {
          * 代码中的类名、方法名、参数名已经指定，请勿修改，直接返回方法规定的值即可
          *
          * @param pHead ListNode类
-         * @param k int整型
+         * @param k     int整型
          * @return ListNode类
          */
         //解法一：双指针法
@@ -33,13 +33,13 @@ public class Jz14 {
             }
             ListNode first = pHead;
             for (int i = 0; i < k; i++) {
-                if (first ==null) {
+                if (first == null) {
                     return null;
                 }
                 first = first.next;
             }
             ListNode last = pHead;
-            while (first!=null){
+            while (first != null) {
                 first = first.next;
                 last = last.next;
             }

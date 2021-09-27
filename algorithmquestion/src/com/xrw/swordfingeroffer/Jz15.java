0299d46ad697bb6f82e3cb3d8d662815185aa9d3
XieRW <1429382875@qq.com>
2021-09-27 14:38:35 +0800

    剑指offer第十五题

---
+++ algorithmquestion/src/com/xrw/swordfingeroffer/Jz15.java
@@ -0,0 +1,31 @@
+package com.xrw.swordfingeroffer;
+
+/**
+ * @program: DataStructures
+ * @description: 剑指offer第十五题
+ * @author: 谢荣旺 1429382875@qq.com
+ * @create: 2021-09-27 14:27
+ **/
+public class Jz15 {
+
+public class ListNode {
+    int val;
+    ListNode next = null;
+
+    ListNode(int val) {
+        this.val = val;
+    }
+}
+    public class Solution {
+        public ListNode ReverseList(ListNode head) {
+            ListNode node = new ListNode(0);
+            while (head!=null){
+                ListNode temp = head;
+                head = head.next;
+                temp.next = node.next;
+                node.next = temp;
+            }
+            return node.next;
+        }
+    }
+}

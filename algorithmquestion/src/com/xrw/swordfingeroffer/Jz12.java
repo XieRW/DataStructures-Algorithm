80270c91422b0ea5d8938377a26b619214bdf79b
XieRW <1429382875@qq.com>
2021-09-27 10:52:29 +0800

    剑指offer第十二题

--- algorithmquestion/src/com/xrw/swordfingeroffer/Jz12.java
+++ algorithmquestion/src/com/xrw/swordfingeroffer/Jz12.java
@@ -9,18 +9,18 @@ package com.xrw.swordfingeroffer;
 public class Jz12 {
     public class Solution {
         public double Power(double base, int exponent) {
-            if (base == 0){
+            if (base == 0) {
                 return 0;
             }
-            if (exponent == 0){
+            if (exponent == 0) {
                 return 1;
             }
             double sum = 1;
             for (int i = 0; i < Math.abs(exponent); i++) {
                 sum *= base;
             }
-            if (exponent<0){
-                sum = 1/sum;
+            if (exponent < 0) {
+                sum = 1 / sum;
             }
             return sum;
         }

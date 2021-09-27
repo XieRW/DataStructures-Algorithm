109a16c5955bb24525868dbc9e161275f3e48f1f
XieRW <1429382875@qq.com>
2021-09-27 11:12:57 +0800

    剑指offer第十三题

--- algorithmquestion/src/com/xrw/swordfingeroffer/Jz13.java
+++ algorithmquestion/src/com/xrw/swordfingeroffer/Jz13.java
@@ -13,28 +13,27 @@ public class Jz13 {
         /**
          * 代码中的类名、方法名、参数名已经指定，请勿修改，直接返回方法规定的值即可
          *
-         *
          * @param array int整型一维数组
          * @return int整型一维数组
          */
-        public int[] reOrderArray (int[] array) {
+        public int[] reOrderArray(int[] array) {
             // write code here
-            if (array.length == 0 || array.length == 1){
+            if (array.length == 0 || array.length == 1) {
                 return array;
             }
             Stack<Integer> stack1 = new Stack<>();
             Stack<Integer> stack2 = new Stack<>();
             for (int i = 0; i < array.length; i++) {
-                if (array[i]%2 == 1){
+                if (array[i] % 2 == 1) {
                     stack1.push(array[i]);
-                }else{
+                } else {
                     stack2.push(array[i]);
                 }
             }
-            for (int i = array.length-1; i >= 0; i--) {
-                if (stack2.size() != 0){
+            for (int i = array.length - 1; i >= 0; i--) {
+                if (stack2.size() != 0) {
                     array[i] = stack2.pop();
-                }else {
+                } else {
                     array[i] = stack1.pop();
                 }
             }

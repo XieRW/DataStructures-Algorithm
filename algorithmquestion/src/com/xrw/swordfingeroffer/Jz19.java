3d4efb5fc26d91c36b3b23e3a893b164dbcf2dfa
XieRW <1429382875@qq.com>
2021-09-28 14:41:38 +0800

    剑指offer第十九题

--- algorithmquestion/src/com/xrw/swordfingeroffer/Jz19.java
+++ algorithmquestion/src/com/xrw/swordfingeroffer/Jz19.java
@@ -12,7 +12,17 @@ import java.util.List;
 public class Jz19 {
 
     public class Solution {
-
+        /**
+         * 简单来说，就是不断地收缩矩阵的边界
+         * 定义四个变量代表范围，up、down、left、right
+         *
+         * 向右走存入整行的值，当存入后，该行再也不会被遍历，代表上边界的 up 加一，同时判断是否和代表下边界的 down 交错
+         * 向下走存入整列的值，当存入后，该列再也不会被遍历，代表右边界的 right 减一，同时判断是否和代表左边界的 left 交错
+         * 向左走存入整行的值，当存入后，该行再也不会被遍历，代表下边界的 down 减一，同时判断是否和代表上边界的 up 交错
+         * 向上走存入整列的值，当存入后，该列再也不会被遍历，代表左边界的 left 加一，同时判断是否和代表右边界的 right 交错
+         * @param matrix 数组
+         * @return
+         */
         public ArrayList<Integer> printMatrix(int [][] matrix) {
             ArrayList<Integer> list = new ArrayList<>();
             if (matrix == null || matrix.length == 0 || matrix[0].length== 0){

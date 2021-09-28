33a51f44e1ea2eabd634de6ec3a44c53c0add0d7
XieRW <1429382875@qq.com>
2021-09-28 14:16:23 +0800

    剑指offer第十八题

--- algorithmquestion/src/com/xrw/swordfingeroffer/Jz18.java
+++ algorithmquestion/src/com/xrw/swordfingeroffer/Jz18.java
@@ -8,27 +8,27 @@ package com.xrw.swordfingeroffer;
  **/
 public class Jz18 {
 
-      public class TreeNode {
+    public class TreeNode {
         int val = 0;
         TreeNode left = null;
         TreeNode right = null;
+
         public TreeNode(int val) {
-          this.val = val;
+            this.val = val;
         }
-      }
+    }
 
 
     public class Solution {
         /**
          * 代码中的类名、方法名、参数名已经指定，请勿修改，直接返回方法规定的值即可
          *
-         *
          * @param pRoot TreeNode类
          * @return TreeNode类
          */
-        public TreeNode Mirror (TreeNode pRoot) {
+        public TreeNode Mirror(TreeNode pRoot) {
             // write code here
-            if (pRoot == null){
+            if (pRoot == null) {
                 return pRoot;
             }
             TreeNode temp = pRoot.left;

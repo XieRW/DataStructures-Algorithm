ec9a3f4295603f948970e245e942b5e3c74b8755
XieRW <1429382875@qq.com>
2021-09-29 14:27:46 +0800

    JZ22 从上往下打印二叉树

--- algorithmquestion/src/com/xrw/swordfingeroffer/Jz22.java
+++ algorithmquestion/src/com/xrw/swordfingeroffer/Jz22.java
@@ -10,34 +10,35 @@ import java.util.ArrayList;
  * @create: 2021-09-29 14:11
  **/
 public class Jz22 {
-     public class TreeNode {
-     int val = 0;
-     TreeNode left = null;
-     TreeNode right = null;
+    public class TreeNode {
+        int val = 0;
+        TreeNode left = null;
+        TreeNode right = null;
 
-     public TreeNode(int val) {
-     this.val = val;
+        public TreeNode(int val) {
+            this.val = val;
 
-     }
+        }
 
-     }
+    }
+
     public class Solution {
-         //思路：广度优先遍历
+        //思路：广度优先遍历
         public ArrayList<Integer> PrintFromTopToBottom(TreeNode root) {
-            if (root == null){
+            if (root == null) {
                 return new ArrayList<Integer>();
             }
             ArrayList<Integer> arrayList = new ArrayList<>();
             ArrayList<TreeNode> node = new ArrayList<>();
             node.add(root);
-            while (!node.isEmpty()){
+            while (!node.isEmpty()) {
                 arrayList.add(node.get(0).val);
                 root = node.get(0);
                 node.remove(0);
-                if (root.left!=null){
+                if (root.left != null) {
                     node.add(root.left);
                 }
-                if (root.right != null){
+                if (root.right != null) {
                     node.add(root.right);
                 }
             }

f79dc9d3bca81b0d42dcd35c03b7f7064b49ad51
XieRW <1429382875@qq.com>
2021-09-29 14:12:17 +0800

    JZ22 从上往下打印二叉树

--- algorithmquestion/src/com/xrw/swordfingeroffer/Jz22.java
+++ algorithmquestion/src/com/xrw/swordfingeroffer/Jz22.java
@@ -1,5 +1,7 @@
 package com.xrw.swordfingeroffer;
 
+import java.util.ArrayList;
+
 /**
  * @program: DataStructures
  * @description: JZ22 从上往下打印二叉树
@@ -8,4 +10,20 @@ package com.xrw.swordfingeroffer;
  * @create: 2021-09-29 14:11
  **/
 public class Jz22 {
+     public class TreeNode {
+     int val = 0;
+     TreeNode left = null;
+     TreeNode right = null;
+
+     public TreeNode(int val) {
+     this.val = val;
+
+     }
+
+     }
+    public class Solution {
+        public ArrayList<Integer> PrintFromTopToBottom(TreeNode root) {
+
+        }
+    }
 }

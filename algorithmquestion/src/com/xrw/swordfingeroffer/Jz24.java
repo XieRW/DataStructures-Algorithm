78cc436ef0548bc8f863aa7c21a7762217c52175
XieRW <1429382875@qq.com>
2021-09-29 15:25:57 +0800

    JZ23 二叉搜索树的后序遍历序列

--- algorithmquestion/src/com/xrw/swordfingeroffer/Jz24.java
+++ algorithmquestion/src/com/xrw/swordfingeroffer/Jz24.java
@@ -1,5 +1,7 @@
 package com.xrw.swordfingeroffer;
 
+import java.util.ArrayList;
+
 /**
  * @program: DataStructures
  * @description: JZ24 二叉树中和为某一值的路径
@@ -7,4 +9,20 @@ package com.xrw.swordfingeroffer;
  * @create: 2021-09-29 15:23
  **/
 public class Jz24 {
+     public class TreeNode {
+     int val = 0;
+     TreeNode left = null;
+     TreeNode right = null;
+
+     public TreeNode(int val) {
+     this.val = val;
+
+     }
+
+     }
+    public class Solution {
+        public ArrayList<ArrayList<Integer>> FindPath(TreeNode root, int target) {
+
+        }
+    }
 }

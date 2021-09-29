5fda89b18240b42a7a891aaacda049c1d18bfa78
XieRW <1429382875@qq.com>
2021-09-29 17:21:15 +0800

    JZ23 二叉搜索树的后序遍历序列

--- algorithmquestion/src/com/xrw/swordfingeroffer/Jz24.java
+++ algorithmquestion/src/com/xrw/swordfingeroffer/Jz24.java
@@ -70,16 +70,17 @@ public class Jz24 {
     public class Solution2 {
         private ArrayList<ArrayList<Integer>> result = new ArrayList<ArrayList<Integer>>();
         private ArrayList<Integer> list = new ArrayList<>();
-        public ArrayList<ArrayList<Integer>> FindPath(TreeNode root,int target) {
-            if(root == null)return result;
+
+        public ArrayList<ArrayList<Integer>> FindPath(TreeNode root, int target) {
+            if (root == null) return result;
             list.add(root.val);
             target -= root.val;
-            if(target == 0 && root.left == null && root.right == null) {
+            if (target == 0 && root.left == null && root.right == null) {
                 result.add(new ArrayList<Integer>(list));
             }
             ArrayList<ArrayList<Integer>> result1 = FindPath(root.left, target);
             ArrayList<ArrayList<Integer>> result2 = FindPath(root.right, target);
-            list.remove(list.size()-1);
+            list.remove(list.size() - 1);
             return result;
         }
     }

06c9f94ae4e06cb19d465901a198d59064c8345b
XieRW <1429382875@qq.com>
2021-09-29 17:19:37 +0800

    JZ23 二叉搜索树的后序遍历序列

--- algorithmquestion/src/com/xrw/swordfingeroffer/Jz24.java
+++ algorithmquestion/src/com/xrw/swordfingeroffer/Jz24.java
@@ -11,52 +11,54 @@ import java.util.ArrayList;
  * @create: 2021-09-29 15:23
  **/
 public class Jz24 {
-     public class TreeNode {
-     int val = 0;
-     TreeNode left = null;
-     TreeNode right = null;
+    public class TreeNode {
+        int val = 0;
+        TreeNode left = null;
+        TreeNode right = null;
 
-     public TreeNode(int val) {
-     this.val = val;
+        public TreeNode(int val) {
+            this.val = val;
 
-     }
+        }
 
-     }
+    }
+
     /**
      * 解法一：回溯法，每次找到叶子节点且路径和满足要求时，添加一个路径list到结果集中，然后通过回溯，把此路径经过的节点添加进去
      */
     public class Solution {
         public ArrayList<ArrayList<Integer>> FindPath(TreeNode root, int target) {
             ArrayList<ArrayList<Integer>> lists = new ArrayList<>();
-            if (root == null){
+            if (root == null) {
                 return lists;
             }
-            helpFindPath(root,target,0,lists);
+            helpFindPath(root, target, 0, lists);
             return lists;
         }
-        public  void helpFindPath(TreeNode root, int target,int now,ArrayList<ArrayList<Integer>> lists){
+
+        public void helpFindPath(TreeNode root, int target, int now, ArrayList<ArrayList<Integer>> lists) {
             now += root.val;
             ArrayList<Integer> list = new ArrayList<>();
-            if (now > target){
-                return ;
+            if (now > target) {
+                return;
             }
-            if (root.left == null && root.right == null){
-                if (now == target){
+            if (root.left == null && root.right == null) {
+                if (now == target) {
                     list.add(root.val);
                     lists.add(list);
                 }
-                return ;
+                return;
             }
-            if (root.left!=null){
-                helpFindPath(root.left,target,now,lists);
+            if (root.left != null) {
+                helpFindPath(root.left, target, now, lists);
 
             }
-            if (root.right!=null){
-                helpFindPath(root.right,target,now,lists);
+            if (root.right != null) {
+                helpFindPath(root.right, target, now, lists);
             }
-            if (!lists.isEmpty()){
+            if (!lists.isEmpty()) {
                 for (int i = 0; i < lists.size(); i++) {
-                    lists.get(i).add(0,root.val);
+                    lists.get(i).add(0, root.val);
                 }
             }
         }

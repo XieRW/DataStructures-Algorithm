afccce01c3bdbd4660f6e04cf1f44364166537cc
XieRW <1429382875@qq.com>
2021-09-29 14:11:58 +0800

    JZ22 从上往下打印二叉树

--- algorithmquestion/src/com/xrw/swordfingeroffer/Jz22.java
+++ algorithmquestion/src/com/xrw/swordfingeroffer/Jz22.java
@@ -3,6 +3,7 @@ package com.xrw.swordfingeroffer;
 /**
  * @program: DataStructures
  * @description: JZ22 从上往下打印二叉树
+ * 描述：从上往下打印出二叉树的每个节点，同层节点从左至右打印。
  * @author: 谢荣旺 1429382875@qq.com
  * @create: 2021-09-29 14:11
  **/

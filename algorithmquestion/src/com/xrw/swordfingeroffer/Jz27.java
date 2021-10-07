0267db69ac318413bb20b25c59298e09077c18c6
XieRW <1429382875@qq.com>
2021-10-07 20:48:49 +0800

    JZ27 字符串的排列

--- algorithmquestion/src/com/xrw/swordfingeroffer/Jz27.java
+++ algorithmquestion/src/com/xrw/swordfingeroffer/Jz27.java
@@ -9,9 +9,35 @@ import java.util.ArrayList;
  **/
 public class Jz27 {
     public class Solution {
-        public ArrayList<String> Permutation(String str) {
-            String[] strs = str.split("");
 
+        public ArrayList<String> PermutationHelp(StringBuilder str){
+            ArrayList<String> result = new  ArrayList<String>();
+            if(str.length() == 1)result.add(str.toString());
+            else{
+                for(int i = 0; i < str.length(); i++){
+                    if(i== 0  || str.charAt(i) != str.charAt(0)){
+                        char temp = str.charAt(i);
+                        str.setCharAt(i, str.charAt(0));
+                        str.setCharAt(0, temp);
+                        ArrayList<String> newResult = PermutationHelp(new StringBuilder(str.substring(1)));
+                        for(int j =0; j < newResult.size(); j++)
+                            result.add(str.substring(0,1)+newResult.get(j));
+                        //用完还是要放回去的
+                        temp = str.charAt(0);
+                        str.setCharAt(0, str.charAt(i));
+                        str.setCharAt(i, temp);
+                    }
+                }
+                //需要在做一个排序操作
+
+            }
+            return result;
+        }
+
+        public ArrayList<String> Permutation(String str) {
+            StringBuilder strBuilder = new StringBuilder(str);
+            ArrayList<String> result = PermutationHelp(strBuilder);
+            return result;
         }
     }
 }

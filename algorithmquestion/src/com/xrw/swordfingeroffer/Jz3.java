90b7e191a1be812a9894edb6dbf87fe01594d3e8
XieRW <1429382875@qq.com>
2021-10-08 10:21:29 +0800

    JZ3 从尾到头打印链表

--- algorithmquestion/src/com/xrw/swordfingeroffer/Jz3.java
+++ algorithmquestion/src/com/xrw/swordfingeroffer/Jz3.java
@@ -6,28 +6,8 @@ import java.util.Stack;
 
 /**
  * @program: DataStructures
- * @description: 牛客网剑指offer第三题
- * 描述
- * 输入一个链表的头节点，按链表从尾到头的顺序返回每个节点的值（用数组返回）。
- * <p>
- * 如输入{1,2,3}的链表如下图:
- * <p>
- * 返回一个数组为[3,2,1]
- * <p>
- * 0 <= 链表长度 <= 10000
- * 示例1
- * 输入：
- * {1,2,3}
- * 复制
- * 返回值：
- * [3,2,1]
- * 复制
- * 示例2
- * 输入：
- * {67,0,24,58}
- * 复制
- * 返回值：
- * [58,24,0,67]
+ * @description: JZ3 从尾到头打印链表
+ * https://www.nowcoder.com/practice/d0267f7f55b3412ba93bd35cfa8e8035?tpId=13&tags=&title=&difficulty=0&judgeStatus=0&rp=0
  * @author: 谢荣旺 1429382875@qq.com
  * @create: 2021-08-12 16:28
  **/

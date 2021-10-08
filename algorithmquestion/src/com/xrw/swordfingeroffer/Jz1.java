5f5d348fdfe28a7328f94b992a62d4a094cb5495
XieRW <1429382875@qq.com>
2021-10-08 10:23:19 +0800

    JZ1 二维数组中的查找

--- algorithmquestion/src/com/xrw/swordfingeroffer/Jz1.java
+++ algorithmquestion/src/com/xrw/swordfingeroffer/Jz1.java
@@ -4,39 +4,8 @@ import java.util.Scanner;
 
 /**
  * @program: DataStructures
- * @description: 牛客网剑指offer第一题 时间复杂度O(log2(mn))
- * 在一个二维数组array中（每个一维数组的长度相同），每一行都按照从左到右递增的顺序排序，每一列都按照从上到下递增的顺序排序。请完成一个函数，输入这样的一个二维数组和一个整数，判断数组中是否含有该整数。
- * [
- * [1,2,8,9],
- * [2,4,9,12],
- * [4,7,10,13],
- * [6,8,11,15]
- * ]
- * 给定 target = 7，返回 true。
- * <p>
- * 给定 target = 3，返回 false。
- * <p>
- * 0 <= array.length <= 500
- * 0 <= array[0].length <= 500
- * <p>
- * 示例1
- * 输入：
- * 7,[[1,2,8,9],[2,4,9,12],[4,7,10,13],[6,8,11,15]]
- * 复制
- * 返回值：
- * true
- * 复制
- * 说明：
- * 存在7，返回true
- * 示例2
- * 输入：
- * 3,[[1,2,8,9],[2,4,9,12],[4,7,10,13],[6,8,11,15]]
- * 复制
- * 返回值：
- * false
- * 复制
- * 说明：
- * 不存在3，返回false
+ * @description: JZ1 二维数组中的查找
+ * https://www.nowcoder.com/practice/abc3fe2ce8e146608e868a70efebf62e?tpId=13&tags=&title=&difficulty=0&judgeStatus=0&rp=0
  * @author: 谢荣旺 1429382875@qq.com
  * @create: 2021-08-12 16:28
  **/

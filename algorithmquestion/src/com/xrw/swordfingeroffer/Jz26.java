e1f981ea35a233d1c73d94bd51e249e84a94ae92
XieRW <1429382875@qq.com>
2021-10-08 09:53:30 +0800

    JZ26 二叉搜索树与双向链表

--- algorithmquestion/src/com/xrw/swordfingeroffer/Jz26.java
+++ algorithmquestion/src/com/xrw/swordfingeroffer/Jz26.java
@@ -6,6 +6,7 @@ import java.util.List;
 /**
  * @program: DataStructures
  * @description: JZ26 二叉搜索树与双向链表
+ * https://www.nowcoder.com/practice/947f6eb80d944a84850b0538bf0ec3a5?tpId=13&tags=&title=&difficulty=0&judgeStatus=0&rp=0
  * @author: 谢荣旺 1429382875@qq.com
  * @create: 2021-10-04 21:19
  **/

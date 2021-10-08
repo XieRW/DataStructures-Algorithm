e465e790dafbd777a8efbcf918772b124234fc85
XieRW <1429382875@qq.com>
2021-10-08 09:56:55 +0800

    JZ16 合并两个排序的链表

--- algorithmquestion/src/com/xrw/swordfingeroffer/Jz16.java
+++ algorithmquestion/src/com/xrw/swordfingeroffer/Jz16.java
@@ -2,7 +2,8 @@ package com.xrw.swordfingeroffer;
 
 /**
  * @program: DataStructures
- * @description: 剑指offer第十六题
+ * @description: JZ16 合并两个排序的链表
+ * https://www.nowcoder.com/practice/d8b6b4358f774294a89de2a6ac4d9337?tpId=13&tags=&title=&difficulty=0&judgeStatus=0&rp=0
  * @author: 谢荣旺 1429382875@qq.com
  * @create: 2021-09-27 14:41
  **/

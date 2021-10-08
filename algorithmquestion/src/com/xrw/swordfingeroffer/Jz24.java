2440a908d8d6e75698d50c20a0b407c10587a2b6
XieRW <1429382875@qq.com>
2021-10-08 09:54:15 +0800

    JZ24 二叉树中和为某一值的路径

--- algorithmquestion/src/com/xrw/swordfingeroffer/Jz24.java
+++ algorithmquestion/src/com/xrw/swordfingeroffer/Jz24.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 /**
  * @program: DataStructures
  * @description: JZ24 二叉树中和为某一值的路径
+ * https://www.nowcoder.com/practice/b736e784e3e34731af99065031301bca?tpId=13&tags=&title=&difficulty=0&judgeStatus=0&rp=0
  * 输入一颗二叉树的根节点和一个整数，按字典序打印出二叉树中结点值的和为输入整数的所有路径。
  * 路径定义为从树的根结点开始往下一直到叶结点所经过的结点形成一条路径。
  * @author: 谢荣旺 1429382875@qq.com

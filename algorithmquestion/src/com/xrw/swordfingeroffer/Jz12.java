4dd9c1382a3b6e391c8324253d6e546288405c2b
XieRW <1429382875@qq.com>
2021-10-08 09:58:44 +0800

    JZ12 数值的整数次方

--- algorithmquestion/src/com/xrw/swordfingeroffer/Jz12.java
+++ algorithmquestion/src/com/xrw/swordfingeroffer/Jz12.java
@@ -2,7 +2,8 @@ package com.xrw.swordfingeroffer;
 
 /**
  * @program: DataStructures
- * @description: 剑指offer第十二题
+ * @description: JZ12 数值的整数次方
+ * https://www.nowcoder.com/practice/1a834e5e3e1a4b7ba251417554e07c00?tpId=13&tags=&title=&difficulty=0&judgeStatus=0&rp=0
  * @author: 谢荣旺 1429382875@qq.com
  * @create: 2021-09-27 10:06
  **/

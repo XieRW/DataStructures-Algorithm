3a5094f729072035fad5be97457f422039b78d5f
XieRW <1429382875@qq.com>
2021-10-08 09:51:08 +0800

    JZ27 字符串的排列

--- algorithmquestion/src/com/xrw/swordfingeroffer/Jz27.java
+++ algorithmquestion/src/com/xrw/swordfingeroffer/Jz27.java
@@ -8,6 +8,16 @@ import java.util.ArrayList;
  * @create: 2021-10-06 18:47
  **/
 public class Jz27 {
+    /**
+     * 总体的解决思路是：
+     * 假设输入为a、b、c
+     * 那么其实排序的总数：
+     * fun（a，b，c）=a（fun（b，c））+ a和b交换（fun（a，c））+a和c交换（fun（b，a））
+     * fun（b，c） = b+fun（c）+b和c交换（fun（b））
+     * fun（c）=1
+     * 所以用递归的方法就可以了，并且在这个递归的过程中，并没有做出一些浪费运行时间的事情，每一个递归都会产生新的结果，因此用递归来解决被称为动态规划的此题，是合理的。
+     * 另外题目中说明可能存在重复的字符，因此在进行交换的时候需要判断进行交换的字符是否相等，如果相等就没有必要交换了。
+     */
     public static class Solution {
 
         public ArrayList<String> permutationHelp(StringBuilder str){

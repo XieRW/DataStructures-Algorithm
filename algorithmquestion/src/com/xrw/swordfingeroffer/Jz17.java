f7c1e8be75a0ca843ae8ca316c045716c4ade776
XieRW <1429382875@qq.com>
2021-10-08 09:56:27 +0800

    JZ17 树的子结构

--- algorithmquestion/src/com/xrw/swordfingeroffer/Jz17.java
+++ algorithmquestion/src/com/xrw/swordfingeroffer/Jz17.java
@@ -2,7 +2,8 @@ package com.xrw.swordfingeroffer;
 
 /**
  * @program: DataStructures
- * @description: 剑指offer第十七题 树的子结构
+ * @description: JZ17 树的子结构
+ * https://www.nowcoder.com/practice/6e196c44c7004d15b1610b9afca8bd88?tpId=13&tags=&title=&difficulty=0&judgeStatus=0&rp=0
  * 输入两棵二叉树A，B，判断B是不是A的子结构。（ps：我们约定空树不是任意一个树的子结构）
  * 输入：{8,8,#,9,#,2,#,5},{8,9,#,2}
  * 输出：true

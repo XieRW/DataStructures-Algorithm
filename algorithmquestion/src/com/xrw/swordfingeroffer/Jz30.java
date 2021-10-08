d3a89987febb8b37e71d3e75c6041a29cc08d527
XieRW <1429382875@qq.com>
2021-10-08 16:03:09 +0800

    JZ30 连续子数组的最大和

--- algorithmquestion/src/com/xrw/swordfingeroffer/Jz30.java
+++ algorithmquestion/src/com/xrw/swordfingeroffer/Jz30.java
@@ -13,20 +13,20 @@ public class Jz30 {
 
     public class Solution {
         public int FindGreatestSumOfSubArray(int[] array) {
-            if (array.length == 0){
+            if (array.length == 0) {
                 return 0;
             }
             int sum = Integer.MIN_VALUE;
             int num = 0;
             for (int i = 0; i < array.length; i++) {
                 sum = Math.max(array[i], sum);
-                if (array[i] >0){
+                if (array[i] > 0) {
                     num += array[i];
                     sum = Math.max(num, sum);
-                }else {
-                    if (num+array[i]>0){
+                } else {
+                    if (num + array[i] > 0) {
                         num += array[i];
-                    }else {
+                    } else {
                         num = 0;
                     }
                 }

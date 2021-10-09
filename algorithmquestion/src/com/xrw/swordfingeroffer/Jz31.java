43dc89795769cd7190412f4be11c4bfec343c088
XieRW <1429382875@qq.com>
2021-10-09 22:44:50 +0800

    JZ31 整数中1出现的次数（从1到n整数中1出现的次数）

---
+++ algorithmquestion/src/com/xrw/swordfingeroffer/Jz31.java
@@ -0,0 +1,16 @@
+package com.xrw.swordfingeroffer;
+
+/**
+ * @program: DataStructures
+ * @description: JZ31 整数中1出现的次数（从1到n整数中1出现的次数）
+ * https://www.nowcoder.com/practice/bd7f978302044eee894445e244c7eee6?tpId=13&tags=&title=&difficulty=0&judgeStatus=0&rp=0
+ * @author: 谢荣旺 1429382875@qq.com
+ * @create: 2021-10-09 22:43
+ **/
+public class Jz31 {
+    public class Solution {
+        public int NumberOf1Between1AndN_Solution(int n) {
+
+        }
+    }
+}

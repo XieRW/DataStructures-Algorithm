0e60403a02ad646cebe765f1188e0e6297c16931
XieRW <1429382875@qq.com>
2021-10-12 16:35:19 +0800

    JZ35 数组中的逆序对 牛客题解：归并，将大问题不断分解成小问题，直到不能再分，解决了最小问题后，再合并较大问题，并解决，再一直合并成最终问题再解决（注意是并起来的时候解决）。因此可以看到，最核心的处理代码，是在合并的时候完成。还有一点要考虑到，如果在统计之后进行排序，可以提高一些效率。（为什么排序会在注释中进行解释说明），合并并且排序，需要使用额外的一个数组，大小与原数组相等，提高了些效率，空间复杂度也提高一丢丢。

--- algorithmquestion/src/com/xrw/swordfingeroffer/Jz35.java
+++ algorithmquestion/src/com/xrw/swordfingeroffer/Jz35.java
@@ -99,7 +99,9 @@ public class Jz35 {
         private int cnt;
 
         private void MergeSort(int[] array, int start, int end) {
-            if (start >= end) return;
+            if (start >= end) {
+                return;
+            }
             int mid = (start + end) / 2;
             MergeSort(array, start, mid);
             MergeSort(array, mid + 1, end);
@@ -111,18 +113,20 @@ public class Jz35 {
             int k = 0, i = start, j = mid + 1;
             while (i <= mid && j <= end) {
 //如果前面的元素小于后面的不能构成逆序对
-                if (array[i] <= array[j])
+                if (array[i] <= array[j]) {
                     temp[k++] = array[i++];
-                else {
+                } else {
 //如果前面的元素大于后面的，那么在前面元素之后的元素都能和后面的元素构成逆序对
                     temp[k++] = array[j++];
                     cnt = (cnt + (mid - i + 1)) % 1000000007;
                 }
             }
-            while (i <= mid)
+            while (i <= mid) {
                 temp[k++] = array[i++];
-            while (j <= end)
+            }
+            while (j <= end) {
                 temp[k++] = array[j++];
+            }
             for (int l = 0; l < k; l++) {
                 array[start + l] = temp[l];
             }

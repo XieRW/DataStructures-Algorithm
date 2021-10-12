cd92e6a6d1bc328ab0c2371628a1c40f56c005ea
XieRW <1429382875@qq.com>
2021-10-12 16:33:37 +0800

    JZ35 数组中的逆序对 牛客题解：[A，B]中的逆序对=[A]的逆序对+[B]中的逆序对+将A，B混排在一起的逆序对

--- algorithmquestion/src/com/xrw/swordfingeroffer/Jz35.java
+++ algorithmquestion/src/com/xrw/swordfingeroffer/Jz35.java
@@ -91,4 +91,43 @@ public class Jz35 {
             return count;
         }
     }
+
+    /**
+     * 牛客题解：[A，B]中的逆序对=[A]的逆序对+[B]中的逆序对+将A，B混排在一起的逆序对
+     */
+    public class Solution3 {
+        private int cnt;
+        private void MergeSort(int[] array, int start, int end){
+            if(start>=end)return;
+            int mid = (start+end)/2;
+            MergeSort(array, start, mid);
+            MergeSort(array, mid+1, end);
+            MergeOne(array, start, mid, end);
+        }
+        private void MergeOne(int[] array, int start, int mid, int end){
+            int[] temp = new int[end-start+1];
+            int k=0,i=start,j=mid+1;
+            while(i<=mid && j<= end){
+//如果前面的元素小于后面的不能构成逆序对
+                if(array[i] <= array[j])
+                    temp[k++] = array[i++];
+                else{
+//如果前面的元素大于后面的，那么在前面元素之后的元素都能和后面的元素构成逆序对
+                    temp[k++] = array[j++];
+                    cnt = (cnt + (mid-i+1))%1000000007;
+                }
+            }
+            while(i<= mid)
+                temp[k++] = array[i++];
+            while(j<=end)
+                temp[k++] = array[j++];
+            for(int l=0; l<k; l++){
+                array[start+l] = temp[l];
+            }
+        }
+        public int InversePairs(int [] array) {
+            MergeSort(array, 0, array.length-1);
+            return cnt;
+        }
+    }
 }

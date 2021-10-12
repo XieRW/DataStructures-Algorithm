8d64a1566fcb49cd0a8ee3f64053cb6c1e166f05
XieRW <1429382875@qq.com>
2021-10-12 10:10:52 +0800

    JZ34 第一个只出现一次的字符 解法一

--- algorithmquestion/src/com/xrw/swordfingeroffer/Jz34.java
+++ algorithmquestion/src/com/xrw/swordfingeroffer/Jz34.java
@@ -1,9 +1,7 @@
 package com.xrw.swordfingeroffer;
 
 import java.util.HashMap;
-import java.util.LinkedList;
 import java.util.Map;
-import java.util.Queue;
 
 /**
  * @program: DataStructures
@@ -12,12 +10,16 @@ import java.util.Queue;
  * @create: 2021-10-12 09:47
  **/
 public class Jz34 {
+
     public class Solution {
+        /**
+         * 解法一：利用Map记录每个字符出现的次数，然后找到第一个出现次数为1的字符所在的位置
+         */
         public int FirstNotRepeatingChar(String str) {
             if (str == null || str.length() == 0){
                 return -1;
             }
-            Map<Character,Integer> map = new HashMap<>();
+            Map<Character,Integer> map = new HashMap<>(256);
             for (int i = 0; i < str.length(); i++) {
                 if (!map.keySet().contains(str.charAt(i))){
                     map.put(str.charAt(i),1);

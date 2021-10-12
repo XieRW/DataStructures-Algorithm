7961f4c57e4e5f8a810fbd4aa8430c48ed922e01
XieRW <1429382875@qq.com>
2021-10-12 17:05:06 +0800

    JZ36 两个链表的第一个公共结点 方法二

--- algorithmquestion/src/com/xrw/swordfingeroffer/Jz36.java
+++ algorithmquestion/src/com/xrw/swordfingeroffer/Jz36.java
@@ -1,6 +1,7 @@
 package com.xrw.swordfingeroffer;
 
 import java.util.HashMap;
+
 /**
  * @program: DataStructures
  * @description: JZ36 两个链表的第一个公共结点
@@ -40,6 +41,62 @@ public class Jz36 {
             return null;
 
         }
+    }
+
+    /**
+     * 方法2：
+     */
+    public ListNode FindFirstCommonNode2(ListNode pHead1, ListNode pHead2) {
+        ListNode current1 = pHead1;
+        ListNode current2 = pHead2;
+        if (pHead1 == null || pHead2 == null) {
+            return null;
+        }
+        int length1 = getLength(current1);
+        int length2 = getLength(current2);
+        // 两连表的长度差
+
+        // 如果链表1的长度大于链表2的长度
+        if (length1 >= length2) {
+            int len = length1 - length2;
+            // 先遍历链表1，遍历的长度就是两链表的长度差
+            while (len > 0) {
+                current1 = current1.next;
+                len--;
+            }
+
+        }
+        // 如果链表2的长度大于链表1的长度
+        else if (length1 < length2) {
+            int len = length2 - length1;
+            // 先遍历链表1，遍历的长度就是两链表的长度差
+            while (len > 0) {
+                current2 = current2.next;
+                len--;
+            }
+
+        }
+        //开始齐头并进，直到找到第一个公共结点
+        while(current1!=current2){
+            current1=current1.next;
+            current2=current2.next;
+        }
+        return current1;
+
+    }
+
+    /**
+     * 求指定链表的长度
+      */
+    public static int getLength(ListNode pHead) {
+        int length = 0;
+
+        ListNode current = pHead;
+        while (current != null) {
+            length++;
+            current = current.next;
+        }
+        return length;
     }
 
 

c347c8e64c2088a28a508296012a779c15b8a592
XieRW <1429382875@qq.com>
2021-10-12 16:30:02 +0800

    JZ35 数组中的逆序对 解法一

--- algorithmquestion/src/com/xrw/swordfingeroffer/Jz35.java
+++ algorithmquestion/src/com/xrw/swordfingeroffer/Jz35.java
@@ -7,9 +7,32 @@ package com.xrw.swordfingeroffer;
  * @create: 2021-10-12 10:15
  **/
 public class Jz35 {
-    public class Solution {
-        public int InversePairs(int [] array) {
-
+    public static void main(String[] args) {
+        System.out.println(Integer.MAX_VALUE);
+        int count = 0;
+        while (count>=0){
+            count++;
         }
+        System.out.println(2147483647+1);
+    }
+    public class Solution {
+
+        public int InversePairs(int [] array) {
+            if (array.length == 0){
+                return 0;
+            }
+            int count = 0;
+            for (int i = 0; i < array.length-1; i++) {
+                for (int j = i+1; j < array.length; j++) {
+                    if (array[i] > array[j]){
+                        count++;
+                        count%=1000000007;
+                    }
+                }
+            }
+            return count;
+        }
+
+
     }
 }

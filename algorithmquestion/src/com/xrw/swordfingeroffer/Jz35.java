e4beafa1131d147944113a4832c606b8f848bfd1
XieRW <1429382875@qq.com>
2021-10-12 16:32:39 +0800

    JZ35 数组中的逆序对 解法二

--- algorithmquestion/src/com/xrw/swordfingeroffer/Jz35.java
+++ algorithmquestion/src/com/xrw/swordfingeroffer/Jz35.java
@@ -15,6 +15,10 @@ public class Jz35 {
         }
         System.out.println(2147483647+1);
     }
+
+    /**
+     * 解法一：遍历整个数组，拿一个元素，依次与后面的元素比较，判断是否是逆序对
+     */
     public class Solution {
 
         public int InversePairs(int [] array) {
@@ -32,7 +36,59 @@ public class Jz35 {
             }
             return count;
         }
+    }
 
+    /**
+     * 解法二：分治算法+归并排序
+     */
+    public class Solution2 {
 
+        public int InversePairs(int [] array) {
+            if (array.length == 0){
+                return 0;
+            }
+            return merge(array,0,array.length-1);
+        }
+
+        public int merge(int[] nums,int start,int end){
+            if (start>=end){
+                return 0;
+            }
+            int count = 0;
+            int mid = (start+end)/2;
+            count += merge(nums,start,mid);
+            count += merge(nums,mid+1,end);
+            int i=start;
+            int j=mid+1;
+            int[] temp = new int[end-start+1];
+            int p=0;
+            while (i<=mid && j<=end){
+                if (nums[i] < nums[j]){
+                    temp[p] = nums[i];
+                    p++;
+                    i++;
+                }else {
+                    count = (count+(mid-i+1))%1000000007;
+                    temp[p] = nums[j];
+                    j++;
+                    p++;
+                }
+            }
+            while (i<=mid){
+                temp[p] = nums[i];
+                p++;
+                i++;
+            }
+            while (j<=end){
+                temp[p]= nums[j];
+                p++;
+                j++;
+            }
+            for (int k = 0; k < temp.length; k++) {
+                nums[start+k] = temp[k];
+            }
+
+            return count;
+        }
     }
 }

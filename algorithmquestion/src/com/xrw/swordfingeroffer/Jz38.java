08ef58f9c0c83d3835ce156ca2622e56b6a7913d
XieRW <1429382875@qq.com>
2021-10-13 10:30:46 +0800

    JZ38 二叉树的深度

---
+++ algorithmquestion/src/com/xrw/swordfingeroffer/Jz38.java
@@ -0,0 +1,27 @@
+package com.xrw.swordfingeroffer;
+
+/**
+ * @program: DataStructures
+ * @description: JZ38 二叉树的深度
+ * @author: 谢荣旺 1429382875@qq.com
+ * @create: 2021-10-13 10:09
+ **/
+public class Jz38 {
+    public class TreeNode {
+        int val = 0;
+        TreeNode left = null;
+        TreeNode right = null;
+
+        public TreeNode(int val) {
+            this.val = val;
+
+        }
+
+    }
+
+    public class Solution {
+        public int TreeDepth(TreeNode root) {
+
+        }
+    }
+}

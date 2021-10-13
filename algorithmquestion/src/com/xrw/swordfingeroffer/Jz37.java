a865d73ceb2e4865f24da7a6a8bfbbdd6ce436b1
XieRW <1429382875@qq.com>
2021-10-13 10:08:46 +0800

    JZ37 数字在升序数组中出现的次数 解法二：二分法，时间复杂度O(log n)

--- algorithmquestion/src/com/xrw/swordfingeroffer/Jz37.java
+++ algorithmquestion/src/com/xrw/swordfingeroffer/Jz37.java
@@ -11,10 +11,10 @@ public class Jz37 {
      * 解法1：遍历数组，时间复杂度O(n)
      */
     public class Solution {
-        public int GetNumberOfK(int [] array , int k) {
+        public int GetNumberOfK(int[] array, int k) {
             int count = 0;
-            for(int e:array){
-                if(k==e) {
+            for (int e : array) {
+                if (k == e) {
                     count++;
                 }
             }
@@ -26,30 +26,30 @@ public class Jz37 {
      * 解法二：二分法，时间复杂度O(log n)
      */
     public class Solution2 {
-        public int GetNumberOfK(int [] array , int k) {
-            if (array.length == 0){
+        public int GetNumberOfK(int[] array, int k) {
+            if (array.length == 0) {
                 return 0;
             }
-            return countHelp(array,0,array.length-1,k);
+            return countHelp(array, 0, array.length - 1, k);
         }
 
-        public int countHelp(int[] array,int start,int end,int k){
-            if (start == end){
-                if (k==array[start]){
+        public int countHelp(int[] array, int start, int end, int k) {
+            if (start == end) {
+                if (k == array[start]) {
                     return 1;
-                }else {
+                } else {
                     return 0;
                 }
             }
-            int count=0;
-            int mid = (start+end)/2;
-            if (k==array[mid]){
-                count+=countHelp(array,start,mid,k);
-                count+=countHelp(array,mid+1,end,k);
-            }else if (k>array[mid]){
-                count+=countHelp(array,mid+1,end,k);
-            }else {
-                count+=countHelp(array,start,mid,k);
+            int count = 0;
+            int mid = (start + end) / 2;
+            if (k == array[mid]) {
+                count += countHelp(array, start, mid, k);
+                count += countHelp(array, mid + 1, end, k);
+            } else if (k > array[mid]) {
+                count += countHelp(array, mid + 1, end, k);
+            } else {
+                count += countHelp(array, start, mid, k);
             }
             return count;
         }

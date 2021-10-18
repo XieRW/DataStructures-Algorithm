11f9ebd58f222b17958e93a99cd1383384570dd8
XieRW <1429382875@qq.com>
2021-10-18 17:46:15 +0800

    二叉搜索树的第k个结点

--- algorithmquestion/src/com/xrw/swordfingeroffer/Jz46.java
+++ algorithmquestion/src/com/xrw/swordfingeroffer/Jz46.java
@@ -1,5 +1,8 @@
 package com.xrw.swordfingeroffer;
 
+import java.util.ArrayList;
+import java.util.Stack;
+
 /**
  * @program: DataStructures
  * @description: 二叉搜索树的第k个结点
@@ -7,7 +10,6 @@ package com.xrw.swordfingeroffer;
  * @create: 2021-10-18 16:58
  **/
 public class Jz46 {
-    /*
     public class TreeNode {
         int val = 0;
         TreeNode left = null;
@@ -19,12 +21,57 @@ public class Jz46 {
         }
 
     }
-    */
+
+    /**
+     * 方法一：用一个列表保存中序遍历结果，然后返回第k个值
+     */
     public class Solution {
         TreeNode KthNode(TreeNode pRoot, int k) {
-
+            if (pRoot == null || k == 0) {
+                return null;
+            }
+            ArrayList<TreeNode> list = new ArrayList<>();
+            DFS(pRoot, list);
+            if (k > list.size()) {
+                return null;
+            }
+            return list.get(k - 1);
         }
 
+        public void DFS(TreeNode pRoot, ArrayList<TreeNode> list) {
+            if (pRoot == null) {
+                return;
+            }
+            DFS(pRoot.left, list);
+            list.add(pRoot);
+            DFS(pRoot.right, list);
+        }
 
+    }
+
+    /**
+     * 利用栈实现非递归的中序遍历
+     */
+    public class Solution2 {
+        TreeNode KthNode(TreeNode pRoot, int k) {
+            if (pRoot == null || k == 0) {
+                return null;
+            }
+            Stack<TreeNode> stack = new Stack<>();
+            TreeNode cur = pRoot;
+            while (!stack.isEmpty() || cur != null) {
+                if (cur != null) {
+                    stack.push(cur);
+                    cur = cur.left;
+                } else {
+                    cur = stack.pop();
+                    if (--k == 0) {
+                        return cur;
+                    }
+                    cur = cur.right;
+                }
+            }
+            return null;
+        }
     }
 }

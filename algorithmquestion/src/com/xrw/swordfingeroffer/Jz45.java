745afa70190e363e26b0b0d1efa6107c70e1adda
XieRW <1429382875@qq.com>
2021-10-18 16:06:28 +0800

    按之字形顺序打印二叉树

--- algorithmquestion/src/com/xrw/swordfingeroffer/Jz45.java
+++ algorithmquestion/src/com/xrw/swordfingeroffer/Jz45.java
@@ -10,5 +10,21 @@ import java.util.ArrayList;
 public class Jz45 {
 
 
+    public class TreeNode {
+        int val = 0;
+        TreeNode left = null;
+        TreeNode right = null;
 
+        public TreeNode(int val) {
+            this.val = val;
+
+        }
+
+    }
+    public class Solution {
+        public ArrayList<ArrayList<Integer> > Print(TreeNode pRoot) {
+
+        }
+
+    }
 }

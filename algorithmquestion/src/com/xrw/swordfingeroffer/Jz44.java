366544f5b184b7c3f119a6a0e934ad8cf17cf45c
XieRW <1429382875@qq.com>
2021-10-18 15:47:20 +0800

    删除链表中重复的结点

--- algorithmquestion/src/com/xrw/swordfingeroffer/Jz44.java
+++ algorithmquestion/src/com/xrw/swordfingeroffer/Jz44.java
@@ -7,5 +7,19 @@ package com.xrw.swordfingeroffer;
  * @create: 2021-10-18 15:46
  **/
 public class Jz44 {
+    /*
+     public class ListNode {
+        int val;
+        ListNode next = null;
 
+        ListNode(int val) {
+            this.val = val;
+        }
+    }
+    */
+    public class Solution {
+        public ListNode deleteDuplication(ListNode pHead) {
+
+        }
+    }
 }

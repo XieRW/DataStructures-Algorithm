995fec146fa8e25d9bb73429dcbebc11863c5346
XieRW <1429382875@qq.com>
2021-10-18 15:24:52 +0800

    链表中环的入口结点

---
+++ algorithmquestion/src/com/xrw/swordfingeroffer/Jz43.java
@@ -0,0 +1,26 @@
+package com.xrw.swordfingeroffer;
+
+/**
+ * @program: DataStructures
+ * @description: 链表中环的入口结点
+ * https://www.nowcoder.com/practice/253d2c59ec3e4bc68da16833f79a38e4
+ * @author: 谢荣旺 1429382875@qq.com
+ * @create: 2021-10-18 15:21
+ **/
+public class Jz43 {
+    public class ListNode {
+        int val;
+        ListNode next = null;
+
+        ListNode(int val) {
+            this.val = val;
+        }
+    }
+
+    public class Solution {
+
+        public ListNode EntryNodeOfLoop(ListNode pHead) {
+
+        }
+    }
+}

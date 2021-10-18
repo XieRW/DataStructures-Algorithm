56c69446c65e707e116d4e6cdbb6b9c2e9f4ef48
XieRW <1429382875@qq.com>
2021-10-18 16:56:10 +0800

    按之字形顺序打印二叉树

--- algorithmquestion/src/com/xrw/swordfingeroffer/Jz45.java
+++ algorithmquestion/src/com/xrw/swordfingeroffer/Jz45.java
@@ -27,25 +27,25 @@ public class Jz45 {
      * Ë«Õ»·¨
      */
     public class Solution {
-        public ArrayList<ArrayList<Integer> > print(TreeNode pRoot) {
+        public ArrayList<ArrayList<Integer>> print(TreeNode pRoot) {
             ArrayList<ArrayList<Integer>> result = new ArrayList<>();
-            if(pRoot == null) {
+            if (pRoot == null) {
                 return result;
             }
             Stack<TreeNode> tmp = new Stack<>();
             Stack<TreeNode> tmp1 = new Stack<>();
             tmp.add(pRoot);
-            while(tmp.size() > 0 || tmp1.size() > 0) {
+            while (tmp.size() > 0 || tmp1.size() > 0) {
                 ArrayList<Integer> flood = new ArrayList<>();
-                if(tmp.size() > 0) {
+                if (tmp.size() > 0) {
                     int size = tmp.size();
-                    for(int i=0; i<size; i++) {
+                    for (int i = 0; i < size; i++) {
                         TreeNode pop = tmp.pop();
                         flood.add(pop.val);
-                        if(pop.left != null) {
+                        if (pop.left != null) {
                             tmp1.add(pop.left);
                         }
-                        if(pop.right != null) {
+                        if (pop.right != null) {
                             tmp1.add(pop.right);
                         }
 
@@ -54,13 +54,13 @@ public class Jz45 {
                     continue;
                 }
                 int size = tmp1.size();
-                for(int i=0; i<size; i++) {
+                for (int i = 0; i < size; i++) {
                     TreeNode pop = tmp1.pop();
                     flood.add(pop.val);
-                    if(pop.right != null) {
+                    if (pop.right != null) {
                         tmp.add(pop.right);
                     }
-                    if(pop.left != null) {
+                    if (pop.left != null) {
                         tmp.add(pop.left);
                     }
                 }

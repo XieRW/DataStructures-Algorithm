67cb654658856d6c69a6b9932aad564e23e25665
XieRW <1429382875@qq.com>
2021-10-18 16:59:06 +0800

    二叉搜索树的第k个结点

--- algorithmquestion/src/com/xrw/swordfingeroffer/Jz46.java
+++ algorithmquestion/src/com/xrw/swordfingeroffer/Jz46.java
@@ -7,5 +7,24 @@ package com.xrw.swordfingeroffer;
  * @create: 2021-10-18 16:58
  **/
 public class Jz46 {
+    /*
+    public class TreeNode {
+        int val = 0;
+        TreeNode left = null;
+        TreeNode right = null;
 
+        public TreeNode(int val) {
+            this.val = val;
+
+        }
+
+    }
+    */
+    public class Solution {
+        TreeNode KthNode(TreeNode pRoot, int k) {
+
+        }
+
+
+    }
 }

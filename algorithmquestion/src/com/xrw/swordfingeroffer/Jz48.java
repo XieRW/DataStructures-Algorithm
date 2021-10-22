882f72fa73e9566bfd3c8e0a862d61cf0a28c45e
XieRW <1429382875@qq.com>
2021-10-22 16:14:38 +0800

    对称的二叉树

--- algorithmquestion/src/com/xrw/swordfingeroffer/Jz48.java
+++ algorithmquestion/src/com/xrw/swordfingeroffer/Jz48.java
@@ -1,5 +1,6 @@
 package com.xrw.swordfingeroffer;
 
+
 /**
  * @program: DataStructures
  * @description: ¶Ô³ÆµÄ¶þ²æÊ÷
@@ -7,5 +8,32 @@ package com.xrw.swordfingeroffer;
  * @create: 2021-10-22 14:59
  **/
 public class Jz48 {
+    public class TreeNode {
+        int val = 0;
+        TreeNode left = null;
+        TreeNode right = null;
+
+        public TreeNode(int val) {
+            this.val = val;
+
+        }
+
+    }
+
+    public class Solution {
+        boolean isSymmetrical(TreeNode pRoot) {
+            return isSame(pRoot.left,pRoot.right);
+        }
+
+        boolean isSame(TreeNode left,TreeNode right){
+            if (left==null&&right==null){
+                return true;
+            }
+            if (left==null||right==null){
+                return false;
+            }
+            return left.val == right.val && isSame(left.left,right.right) && isSame(right.left,left.right);
+        }
+    }
 
 }

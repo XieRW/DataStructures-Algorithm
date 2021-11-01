067fe0cc9a10be00a3ccf8b2cee423007b5d1f8a
XieRW <1429382875@qq.com>
2021-11-01 15:49:17 +0800

    滑动窗口的最大值

--- algorithmquestion/src/com/xrw/swordfingeroffer/Jz52.java
+++ algorithmquestion/src/com/xrw/swordfingeroffer/Jz52.java
@@ -1,6 +1,7 @@
 package com.xrw.swordfingeroffer;
 
 import java.util.*;
+
 /**
  * @program: DataStructures
  * @description: 滑动窗口的最大值
@@ -12,19 +13,20 @@ public class Jz52 {
      * 第一种方法，暴力递归
      */
     public class Solution {
-        public ArrayList<Integer> maxInWindows(int [] num, int size) {
+        public ArrayList<Integer> maxInWindows(int[] num, int size) {
             ArrayList<Integer> list = new ArrayList<>();
-            if (size==0 || size>num.length){
+            if (size == 0 || size > num.length) {
                 return list;
             }
-            for (int i = 0; i <= num.length-size; i++) {
+            for (int i = 0; i <= num.length - size; i++) {
                 int[] ints = Arrays.copyOfRange(num, i, size + i);
                 Arrays.sort(ints);
-                list.add(ints[ints.length-1]);
+                list.add(ints[ints.length - 1]);
             }
             return list;
         }
     }
+
     /**
      * 第二种方法，双端队列
      * 遍历数组的每一个元素，
@@ -33,21 +35,21 @@ public class Jz52 {
      * 如果当前元素小于容器的最后一个元素，则直接将当前元素加入到容器的末尾
      * 如果容器头部的元素已经不属于当前窗口的边界，则应该将头部元素删除
      */
-    public ArrayList<Integer> maxInWindows(int [] num, int size) {
+    public ArrayList<Integer> maxInWindows(int[] num, int size) {
         ArrayList<Integer> list = new ArrayList<>();
-        if (size==0 || size>num.length){
+        if (size == 0 || size > num.length) {
             return list;
         }
         Deque<Integer> deque = new LinkedList<>();
         for (int i = 0; i <= num.length; i++) {
-            while (!deque.isEmpty() && num[deque.peekLast()]<num[i]){
+            while (!deque.isEmpty() && num[deque.peekLast()] < num[i]) {
                 deque.pollLast();
             }
             deque.offerLast(i);
-            if (deque.peekFirst()+size<=i){
+            if (deque.peekFirst() + size <= i) {
                 deque.pollFirst();
             }
-            if (i+1>=size){
+            if (i + 1 >= size) {
                 list.add(num[deque.peekFirst()]);
             }
         }

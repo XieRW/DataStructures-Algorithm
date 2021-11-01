11e0849db5edba96529b0a163fb97edf4b3b0641
XieRW <1429382875@qq.com>
2021-11-01 10:31:30 +0800

    序列化二叉树

---
+++ algorithmquestion/src/com/xrw/swordfingeroffer/Jz50.java
@@ -0,0 +1,29 @@
+package com.xrw.swordfingeroffer;
+
+/**
+ * @program: DataStructures
+ * @description: ÐòÁÐ»¯¶þ²æÊ÷
+ * @author: Ð»ÈÙÍú 1429382875@qq.com
+ * @create: 2021-11-01 10:30
+ **/
+public class Jz50 {
+public class TreeNode {
+    int val = 0;
+    TreeNode left = null;
+    TreeNode right = null;
+
+    public TreeNode(int val) {
+        this.val = val;
+
+    }
+
+}
+    public class Solution {
+        String Serialize(TreeNode root) {
+
+        }
+        TreeNode Deserialize(String str) {
+
+        }
+    }
+}

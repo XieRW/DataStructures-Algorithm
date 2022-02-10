29ea217ff90463002deaaac5db28da5b0d323a46
xieRW <37648592+XieRW@users.noreply.github.com>
2022-02-11 00:04:18 +0800

    Update test1.java

--- algorithmquestion/src/test/date211129/test1.java
+++ algorithmquestion/src/test/date211129/test1.java
@@ -20,7 +20,6 @@ public class test1 {
     /**
      * 思路：
      * 1、   p、q应该在root的异侧，否则不为最近公共根节点  或者p为root，q在p的子树中，或者q为root，p在q的子树中；
-     * 2、
      */
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
         if(root == null || root == p || root == q){
